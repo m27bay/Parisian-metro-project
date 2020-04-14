2eaf11ce38550f176379069e51ddb3fb911e0d2a
m27bay <mickael.ledenmat@gmail.com>
2020-04-14 14:17:31 +0200

    Change indentation

--- dijkstra/Matrice.java
+++ dijkstra/Matrice.java
@@ -5,45 +5,62 @@
  * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
+
 public class Matrice
 {
-  private int[][] directions;//line*column
+  //line*column
+  private int[][] directions;
+
+  //
   private int line;
   private int column;
+
   /**
-   * Constructeur d'objets de classe Matrice
-   */
+    * Constructeur d'objets de classe Matrice
+    */
   public Matrice(int[][] M)
   {
-      this.directions = M;
-      this.column = directions[0].length;
-      this.line = directions.length;
+    this.directions = M;
+    this.column = directions[0].length;
+    this.line = directions.length;
   }
-
-  public Matrice(){
-      this.directions = null;
-      this.line = this.column = 0;
-  }
-
-
-  public int getColumn(){return this.column;}
-
-
-  public int getLine(){return this.line;}
-
-  public int getVal(int i, int j){return this.directions[i][j];}
 
   /**
-   * affiche la matrice
-   */
-  public void printMatrice(){
-      int i, j;
-      System.out.println("format : "+this.line+"X"+this.column);
-      for(i=0; i<line; i++){
-          for(j=0; j<column; j++){
-              System.out.print(directions[i][j]+" ");
-          }
-          System.out.print("\n");
-      }
+    *
+    */
+  public Matrice()
+  {
+    this.directions = null;
+    this.line = this.column = 0;
+  }
+
+  /**
+    *
+    */
+  public int getColumn() { return this.column; }
+
+  /**
+    *
+    */
+  public int getLine() { return this.line; }
+
+  /**
+    *
+    */
+  public int getVal(int i, int j) { return this.directions[i][j]; }
+
+  /**
+    * Affiche la matrice
+    */
+  public void printMatrice()
+  {
+    int i, j;
+    System.out.println("format : "+this.line+"X"+this.column);
+    for(i=0; i<line; i++)
+    {
+      for(j=0; j<column; j++)
+          System.out.print(directions[i][j]+" ");
+      System.out.print("\n");
+    }
   }
 }

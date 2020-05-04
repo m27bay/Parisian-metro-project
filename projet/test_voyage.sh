8b6644b9e04441af48674ae2041ce503175c5e89
m27bay <mickael.ledenmat@gmail.com>
2020-05-04 19:13:06 +0200

    Update

--- projet/test_voyage.sh
+++ projet/test_voyage.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
-rm -f ../save.txt
-touch ../save.txt
+file="../save.txt"
+rm -f $file
+touch $file
 
-for i in `seq 0 10`
-do
-  java -cp commons-lang3-3.9/commons-lang3-3.9.jar:. Main random
-  cat ../UserTravel.txt >> ../save.txt
-  echo "" >> ../save.txt
-  echo "" >> ../save.txt
-done
+if [ $# -eq 0 ]
+then
+  echo "Usage : number_test"
+else
 
-sublime ../save.txt
+  #
+  if [ $1 -ge 0 ]
+  then
+
+    #
+    for i in `seq 0 $1`
+    do
+      java -cp commons-lang3-3.9/commons-lang3-3.9.jar:. Main random
+      cat ../UserTravel.txt >> $file
+      echo "" >> $file
+      echo "" >> $file
+    done
+    sublime $file
+
+  #
+  else
+    echo "Usage : number_test ( <= 0 )"
+
+  fi
+fi
